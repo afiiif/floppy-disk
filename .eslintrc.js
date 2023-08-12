5874c8e40caef9d37e093b785946df5ba0c21f6c
M. Afifudin <afif.x.udin@gmail.com>
2023-08-13 04:34:02 +0700

    chore: initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+};
