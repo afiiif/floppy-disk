1c4ef9ded2ba239c3bbf0a5ff98b432823655c6d
M. Afifudin <afif.x.udin@gmail.com>
2023-08-16 23:42:52 +0700

    chore: prepare examples & comparison

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': 'eslint --fix --quiet',
-  '*.{js,jsx,ts,tsx,css,scss,json,html,md,mdx}': 'prettier --write',
-  '*.{ts,tsx}': () => 'tsc --noEmit --skipLibCheck',
+  '!((comparison|examples)/**)*.{js,jsx,ts,tsx}': 'eslint --fix --quiet',
+  '!((comparison|examples)/**)*.{js,jsx,ts,tsx,css,scss,json,html,md,mdx}': 'prettier --write',
+  '!((comparison|examples)/**)*.{ts,tsx}': () => 'tsc --noEmit --skipLibCheck',
 };
