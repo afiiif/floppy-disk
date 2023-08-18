ad40540aa94d688ac399a7f6cb6f54eaebb3ca68
M. Afifudin <afif.x.udin@gmail.com>
2023-08-18 08:30:29 +0700

    chore(eslint): add no-shadow to rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['react-app', 'prettier'],
   plugins: ['simple-import-sort'],
   rules: {
+    '@typescript-eslint/no-shadow': 'warn',
     'simple-import-sort/imports': 'error',
     // 'simple-import-sort/exports': 'error',
   },
