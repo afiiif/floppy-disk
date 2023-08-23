a06b80b8138c2a40b05079f6c20d4c51ca8cab99
M. Afifudin <afif.x.udin@gmail.com>
2023-08-23 08:37:31 +0700

    chore(example): add query & mutation example

---
+++ examples/react/mutation/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { createMutation } from 'floppy-disk';
+import { useRef } from 'react';
+
+const useLoginMutation = createMutation(
+  async ({ email, password }) => {
+    const res = await fetch('https://reqres.in/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+      mode: 'cors',
+    });
+    const resJson = await res.json();
+    if (res.ok) return resJson;
+    throw resJson;
+  },
+  {
+    onSuccess: (response, variables) => {
+      console.log(`Logged in as ${variables.email}`);
+      console.log(`Access token: ${response.token}`);
+    },
+  },
+);
+
+function App() {
+  const { mutate, isWaiting, error } = useLoginMutation();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  return (
+    <main>
+      <h1>💾 Floppy Disk - Mutation</h1>
+
+      <input type="email" ref={emailRef} defaultValue="eve.holt@reqres.in" />
+      <input type="password" ref={passwordRef} />
+      <div>{error?.error}</div>
+
+      <button
+        disabled={isWaiting}
+        onClick={() => {
+          mutate({ email: emailRef.current.value, password: passwordRef.current.value }).then(
+            ({ response, error }) => {
+              if (error) {
+                alert(`Login failed\n${JSON.stringify(error, null, 2)}`);
+              } else {
+                alert('Login success');
+              }
+            },
+          );
+        }}
+      >
+        Login
+      </button>
+    </main>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
