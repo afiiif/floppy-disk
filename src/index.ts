0c98bb8b2c647d81afc4fa5fd4e52ef7a1ad0915
M. Afifudin <afif.x.udin@gmail.com>
2023-08-27 12:02:00 +0700

    chore: update barrel export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 export * from './vanilla';
-
-export * from './react/create-store';
-export * from './react/create-stores';
-export * from './react/create-query';
-export * from './react/create-mutation';
-export * from './react/with-context';
+export * from './react';
---
+++ src/react/index.ts
@@ -0,0 +1,5 @@
+export * from './create-store';
+export * from './create-stores';
+export * from './create-query';
+export * from './create-mutation';
+export * from './with-context';
