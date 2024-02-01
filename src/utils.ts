5a471db2a5f6cc6c1ed9c5dd778acf0c0b1195ce
M. Afifudin <afif.x.udin@gmail.com>
2024-02-01 07:35:55 +0700

    docs: update JSDoc

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,27 @@
+export type Maybe<T> = T | null | undefined;
+
+/**
+ * Check if this runs on browser.
+ */
+export const isClient = typeof window !== 'undefined' && !('Deno' in window);
+
 export const noop = () => {};
+
 export const identityFn = <T>(value: T) => value;
+
+/**
+ * Check if a value is not undefined and not null.
+ *
+ * ```ts
+ * const hasValue = (value: any) => value !== undefined && value !== null;
+ * ```
+ */
 export const hasValue = (value: any) => value !== undefined && value !== null;
 
+/**
+ * If the value is a function, it will invoke the function.\
+ * If the value is not a function, it will just return it.
+ */
 export const getValue = <T, P extends any[]>(
   valueOrComputeValueFn: T | ((...params: P) => T),
   ...params: P
@@ -12,10 +32,9 @@ export const getValue = <T, P extends any[]>(
   return valueOrComputeValueFn;
 };
 
-export type Maybe<T> = T | null | undefined;
-
-export const isClient = typeof window !== 'undefined' && !('Deno' in window);
-
+/**
+ * Create an Error instance with custom props.
+ */
 export const createError = (message: string, props: Record<string, any>) => {
   const error = Object.assign(new Error(message), props);
   return error;
