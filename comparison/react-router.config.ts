87e5ff9bf98d2c873b916f7411f79dfa7f011bdd
M. Afifudin <afif.x.udin@gmail.com>
2026-04-01 00:40:34 +0700

    build: deploy GitHub page

--- comparison/react-router.config.ts
+++ comparison/react-router.config.ts
@@ -3,5 +3,8 @@ import type { Config } from '@react-router/dev/config';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  async prerender() {
+    return ['/', '/store/zustand', '/store/floppy-disk', '/async/tanstack', '/async/floppy-disk'];
+  },
 } satisfies Config;
