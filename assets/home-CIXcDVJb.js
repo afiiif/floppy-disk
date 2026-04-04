6fa0d1933adc3bdb8a94766900989104bdd7a080
afiiif <43276538+afiiif@users.noreply.github.com>
2026-04-04 11:12:09 +0000

    Deploying to gh-pages from @ afiiif/floppy-disk@513b4d5a6621faac850ba423037cba16cbc4e053 🚀

---
+++ assets/home-CIXcDVJb.js
@@ -0,0 +1,17 @@
+import{w as n,p as i}from"./chunk-UVKPFVEO-BCVTWmlK.js";function p(){return[{title:"FloppyDisk.ts Comparison"},{name:"description",content:"FloppyDisk.ts comparison with Zustand & TanStack-Query"}]}const l=`✓ 103 modules transformed.
+build/client/.vite/manifest.json                     3.23 kB │ gzip:  0.60 kB
+build/client/a____s/root-DS469DaY.css               15.33 kB │ gzip:  3.74 kB
+build/client/a____s/components-DrxDq1-G.js           1.01 kB │ gzip:  0.59 kB
+build/client/a____s/store-floppy-disk-D634Gohp.js    1.13 kB │ gzip:  0.52 kB
+build/client/a____s/store-zustand-BJYVMYXp.js        1.68 kB │ gzip:  0.79 kB
+build/client/a____s/utils-DpvILWFZ.js                1.95 kB │ gzip:  0.60 kB
+build/client/a____s/root-CB2-KCWF.js                 2.48 kB │ gzip:  1.01 kB
+build/client/a____s/home-DkCzTYiQ.js                 2.81 kB │ gzip:  1.19 kB
+build/client/a____s/async-floppy-disk-C1Jq5CIc.js    7.15 kB │ gzip:  1.99 kB
+build/client/a____s/react-BaGhX6Iy.js               11.44 kB │ gzip:  3.87 kB
+build/client/a____s/async-tanstack-Bqi-YPia.js      43.94 kB │ gzip: 12.62 kB
+build/client/a____s/chunk-UVKPFVEO-BCVTWmlK.js     126.29 kB │ gzip: 42.59 kB
+build/client/a____s/entry.client-CMKBzKJ-.js       190.57 kB │ gzip: 60.05 kB
+✓ built in 715ms`.split(`
+`),d=n(function(){return i.jsxs("div",{className:"leading-[20px]",children:[i.jsxs("p",{className:"pb-6",children:["I built a library that ",i.jsx("strong",{children:"could replace"})," Zustand and TanStack-Query."," ",i.jsx("span",{className:"inline-block",children:"Bold claim, I know."})," Let's compare!"]}),i.jsx("section",{className:"text-[11px] sm:text-[13px] leading-[15px] sm:leading-[18px] border p-3 rounded overflow-x-auto",children:i.jsx("div",{className:"whitespace-pre min-w-[467px] space-y-1",children:l.map((e,a)=>{let s,t="";return e.includes("floppy-disk")?(s="bg-green-500/10 text-green-300",t=" 🎉"):(e.includes("zustand")||e.includes("tanstack"))&&(s="bg-orange-500/15 text-orange-400"),i.jsxs("div",{className:s,children:[e.replace("____s","ssets").replace("build/client","  "),t]},a)})})}),i.jsxs("p",{className:"pt-6",children:["This site focuses on comparing bundle size while demonstrating"," ",i.jsx("span",{className:"inline-block font-bold",children:"equivalent functionality"}),"."]}),i.jsx("div",{className:"pt-5",children:"You can find the code here:"}),i.jsx("a",{className:"link",href:"https://github.com/afiiif/floppy-disk/tree/beta/comparison",children:"github.com/afiiif/floppy-disk/tree/beta/comparison"}),i.jsx("div",{className:"pt-5",children:"Documentation is available here:"}),i.jsx("a",{className:"link",href:"https://github.com/afiiif/floppy-disk/tree/beta#readme",children:"github.com/afiiif/floppy-disk/tree/beta#readme"})]})});export{d as default,p as meta};
+//# sourceMappingURL=home-CIXcDVJb.js.map
