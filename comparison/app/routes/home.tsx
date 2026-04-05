954a18ae655e90cf14ebd7f6e44a416fd6366450
M. Afifudin <afif.x.udin@gmail.com>
2026-04-05 23:30:38 +0700

    chore(comparison): bump version & update docs link

--- comparison/app/routes/home.tsx
+++ comparison/app/routes/home.tsx
@@ -10,13 +10,13 @@ const buildOutput = `✓ 103 modules transformed.
 build/client/.vite/manifest.json                     3.23 kB │ gzip:  0.60 kB
 build/client/a____s/root-DS469DaY.css               15.33 kB │ gzip:  3.74 kB
 build/client/a____s/components-DrxDq1-G.js           1.01 kB │ gzip:  0.59 kB
-build/client/a____s/store-floppy-disk-D634Gohp.js    1.13 kB │ gzip:  0.52 kB
+build/client/a____s/store-floppy-disk-yMq1m5Ah.js    1.13 kB │ gzip:  0.51 kB
 build/client/a____s/store-zustand-BJYVMYXp.js        1.68 kB │ gzip:  0.79 kB
 build/client/a____s/utils-DpvILWFZ.js                1.95 kB │ gzip:  0.60 kB
 build/client/a____s/root-CB2-KCWF.js                 2.48 kB │ gzip:  1.01 kB
-build/client/a____s/home-DkCzTYiQ.js                 2.81 kB │ gzip:  1.19 kB
-build/client/a____s/async-floppy-disk-C1Jq5CIc.js    7.15 kB │ gzip:  1.99 kB
-build/client/a____s/react-BaGhX6Iy.js               11.44 kB │ gzip:  3.87 kB
+build/client/a____s/home-C8prno_k.js                 2.77 kB │ gzip:  1.19 kB
+build/client/a____s/async-floppy-disk-BBLXq8EL.js    7.15 kB │ gzip:  1.99 kB
+build/client/a____s/react-DhFcpcuC.js               11.47 kB │ gzip:  3.88 kB
 build/client/a____s/async-tanstack-Bqi-YPia.js      43.94 kB │ gzip: 12.62 kB
 build/client/a____s/chunk-UVKPFVEO-BCVTWmlK.js     126.29 kB │ gzip: 42.59 kB
 build/client/a____s/entry.client-CMKBzKJ-.js       190.57 kB │ gzip: 60.05 kB
@@ -62,8 +62,8 @@ export default function Home() {
       </a>
 
       <div className="pt-5">Documentation is available here:</div>
-      <a className="link" href="https://github.com/afiiif/floppy-disk/tree/beta#readme">
-        github.com/afiiif/floppy-disk/tree/beta#readme
+      <a className="link" href="https://floppy-disk.vercel.app">
+        floppy-disk.vercel.app
       </a>
     </div>
   );
