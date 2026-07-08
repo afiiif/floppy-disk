16f19756e39f44e4838f5e37e2221fbcd5e29074
afiiif <43276538+afiiif@users.noreply.github.com>
2026-07-08 13:17:26 +0000

    Deploying to gh-pages from @ afiiif/yuustate@20370d6392d1a9ec7dcc1b3b92fc56328aabac52 🚀

---
+++ assets/home-DTiNyCnA.js
@@ -0,0 +1,17 @@
+import{w as n,p as e}from"./chunk-UVKPFVEO-BCVTWmlK.js";function r(){return[{title:"YuuState Comparison"},{name:"description",content:"YuuState comparison with Zustand & TanStack-Query"}]}const l=`✓ 103 modules transformed.
+build/client/.vite/manifest.json                  3.20 kB │ gzip:  0.60 kB
+build/client/a____s/root-CRn6MgiL.css            15.45 kB │ gzip:  3.77 kB
+build/client/a____s/components-DrxDq1-G.js        1.01 kB │ gzip:  0.59 kB
+build/client/a____s/store-yuustate-ANnGAsrD.js    1.12 kB │ gzip:  0.51 kB
+build/client/a____s/store-zustand-BJYVMYXp.js     1.68 kB │ gzip:  0.79 kB
+build/client/a____s/utils-DuanyUhp.js             1.94 kB │ gzip:  0.60 kB
+build/client/a____s/root-CDn2kOZF.js              2.46 kB │ gzip:  1.01 kB
+build/client/a____s/home-DQiCh3EF.js              2.74 kB │ gzip:  1.18 kB
+build/client/a____s/async-yuustate-DoDkgSCs.js    7.13 kB │ gzip:  1.99 kB
+build/client/a____s/react-yNhF9ZRY.js            11.80 kB │ gzip:  4.02 kB
+build/client/a____s/async-tanstack-Cq-pcNs-.js   43.94 kB │ gzip: 12.62 kB
+build/client/a____s/chunk-UVKPFVEO-BCVTWmlK.js  126.29 kB │ gzip: 42.59 kB
+build/client/a____s/entry.client-CMKBzKJ-.js    190.57 kB │ gzip: 60.05 kB
+✓ built in 715ms`.split(`
+`),o=n(function(){return e.jsxs("div",{className:"leading-[20px]",children:[e.jsxs("p",{className:"pb-6",children:["I built a library that ",e.jsx("strong",{children:"could replace"})," Zustand and TanStack-Query."," ",e.jsx("span",{className:"inline-block",children:"Bold claim, I know."})," Let's compare!"]}),e.jsx("section",{className:"text-[11px] sm:text-[13px] leading-[15px] sm:leading-[18px] border p-3 rounded overflow-x-auto",children:e.jsx("div",{className:"whitespace-pre min-w-[467px] space-y-1",children:l.map((s,a)=>{let i,t="";return s.includes("yuustate")?(i="bg-green-500/10 text-green-300",t=" 🎉"):(s.includes("zustand")||s.includes("tanstack"))&&(i="bg-orange-500/15 text-orange-400"),e.jsxs("div",{className:i,children:[s.replace("____s","ssets").replace("build/client","  "),t]},a)})})}),e.jsxs("p",{className:"pt-6",children:["This site focuses on comparing bundle size while demonstrating"," ",e.jsx("span",{className:"inline-block font-bold",children:"equivalent functionality"}),"."]}),e.jsx("div",{className:"pt-5",children:"You can find the code here:"}),e.jsx("a",{className:"link",href:"https://github.com/afiiif/yuustate/tree/main/comparison",children:"github.com/afiiif/yuustate/tree/main/comparison"}),e.jsx("div",{className:"pt-5",children:"Documentation is available here:"}),e.jsx("a",{className:"link",href:"https://yuustate.vercel.app",children:"yuustate.vercel.app"})]})});export{o as default,r as meta};
+//# sourceMappingURL=home-DTiNyCnA.js.map
